b403de4433696039ae60032baac3105e661319c1
Kazufumi Onuma <onuma.kazufumi@canon.co.jp>
2016-05-17 11:16:48 +0900

    Add: linkfile

---
+++ dotfileLink.bat
@@ -0,0 +1,4 @@
+mklink ..\.vimrc %~dp0.vimrc
+mklink ..\.tmux.conf %~dp0.tmux.conf
+mklink ..\.gitconfig %~dp0.gitconfig
+
---
+++ dotfileLink.sh
@@ -0,0 +1,4 @@
+ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
+ln -s ~/dotfiles/.vimrc ~/.vimrc
+ln -s ~/dotfiles/.gitconfig ~/.gitconfig
+
